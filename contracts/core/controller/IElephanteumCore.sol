c3d9a7cc1598514ed248d276c7a591203ef8d188
milkho <michael.khoroshun@gmail.com>
2018-01-01 12:11:14 +0200

    Set up for using proxy

--- contracts/core/controller/IElephanteumCore.sol
+++ contracts/core/controller/IElephanteumCore.sol
@@ -3,17 +3,20 @@ pragma solidity ^0.4.18;
 
 interface IElephanteumCore {
 
+    function init(bytes32 name, bytes32 symbol, uint supply) external;
+
     function getElephant(address to, uint elephantIndex) external;
     function transferElephant(address from, address to, uint elephantIndex) external;
     
     function offerForSale(address from, uint elephantIndex, uint minSalePriceInWei, address toAddress) external;
     function setNoLongerForSale(address from, uint elephantIndex) external;
 
-    function enterBid(address from, uint elephantIndex) external payable;
+    function enterBid(address from, uint elephantIndex) payable external;
     function acceptBid(address from, uint elephantIndex, uint minPrice) external;
 
     function withdrawBid(address to, uint elephantIndex) external;
     function withdraw(address to) external;
 
     function transferStorage(address newCore) external;
+
 }
---
+++ contracts/core/proxy/ElephanteumProxy.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.4.18;
+
+
+import "../common/Ownable.sol";
+import "../controller/IElephanteumCore.sol";
+import "../storage/ElephanteumStorage.sol";
+
+
+contract ElephanteumProxy is Ownable {
+
+    IElephanteumCore public eCore;
+
+    function ElephanteumProxy(IElephanteumCore _eCore) public payable {
+        eCore = IElephanteumCore(_eCore);     
+    }
+
+    function init(bytes32 name, bytes32 symbol, uint supply) public onlyOwner {
+        eCore.init(name, symbol, supply);
+    }
+
+    function getElephant(uint elephantIndex) public {
+        eCore.getElephant(msg.sender, elephantIndex);
+    }
+
+    function transferElephant(address to, uint elephantIndex) public {
+        eCore.transferElephant(msg.sender,  to,  elephantIndex);
+    }
+
+    function offerForSale(uint elephantIndex, uint minPrice, address to) public {
+        eCore.offerForSale(msg.sender,  elephantIndex,  minPrice, to);
+    }
+
+    function setNoLongerForSale(uint elephantIndex) public {
+        eCore.setNoLongerForSale(msg.sender,  elephantIndex);
+    }
+
+    function enterBid(uint elephantIndex) public payable {
+        eCore.enterBid.value(msg.value)(msg.sender, elephantIndex);
+    }
+
+    function acceptBid(uint elephantIndex, uint minPrice) public {
+        eCore.acceptBid(msg.sender, elephantIndex, minPrice);
+    }
+
+    function withdraw() public {
+        eCore.withdraw(msg.sender);
+    }
+
+    function withdrawBid(uint elephantIndex) public {
+        eCore.withdrawBid(msg.sender, elephantIndex);
+    }
+
+    function transferStorage(address newCore) onlyOwner external {
+        eCore.transferStorage(newCore);
+    }
+}
