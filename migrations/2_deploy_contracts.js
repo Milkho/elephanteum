f2bce214b5bf4010c5258272ff5239b254ed1694
milkho <michael.khoroshun@gmail.com>
2018-01-04 21:05:13 +0200

    Update truffle deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,25 @@
-var Ownable = artifacts.require("Ownable");
-var ElephanteumCore = artifacts.require("ElephanteumCore");
-var ElephanteumStorage = artifacts.require("ElephanteumStorage");
-var ElephanteumAdvancedCore = artifacts.require("ElephanteumAdvancedCore");
+var ElephanteumController = artifacts.require("ElephanteumController");
 var ElephanteumProxy = artifacts.require("ElephanteumProxy");
+var Storage = artifacts.require("Storage");
+var StorageInterface = artifacts.require("StorageInterface");
+var StorageAdapter = artifacts.require("StorageAdapter");
+var BidsRegistry = artifacts.require("BidsRegistry");
+var LotsRegistry = artifacts.require("LotsRegistry");
+var WithdrawalsRegistry = artifacts.require("WithdrawalsRegistry");
+var ElephanteumRegistry = artifacts.require("ElephanteumRegistry");
+var StorageAccessManager = artifacts.require("StorageAccessManager");
+
 
 module.exports = async (deployer, network, accounts) => {
-  const owner = accounts[0];
-  deployer.deploy(Ownable, { from: owner });
-  deployer.link(Ownable, ElephanteumStorage);
-  deployer.link(Ownable, ElephanteumCore);
-  await deployer.deploy(ElephanteumStorage, { from: owner }); 
-  await deployer.deploy(ElephanteumCore, ElephanteumStorage.address, { from: owner });
-  deployer.link(Ownable, ElephanteumAdvancedCore);
-  deployer.deploy(ElephanteumAdvancedCore, ElephanteumStorage.address, { from: owner });
-  deployer.link(Ownable, ElephanteumProxy);
-  deployer.deploy(ElephanteumProxy, ElephanteumCore.address, { from: owner })
+  
+  await deployer.deploy(StorageAccessManager);
+  await deployer.deploy(StorageInterface);
+  await deployer.deploy(Storage);
+  
+  await deployer.deploy(BidsRegistry, Storage.address, "BidRegistry");
+  await deployer.deploy(LotsRegistry, Storage.address, "LotRegistry");
+  await deployer.deploy(WithdrawalsRegistry, Storage.address, "WithdrawalRegistry");
+  await deployer.deploy(ElephanteumRegistry, Storage.address, "ElephanteumRegistry");
+
+  await deployer.deploy(ElephanteumController);
 };
